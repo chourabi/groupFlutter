76a397f8df79f9f92e62e17830a675b4bc688855
taher_chourabi <tchourabi@gmail.com>
2021-02-28 16:58:13 +0100

    first week

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    int nbrContainerPerLine = 4;
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("formation"),
+      ),
+      body: new SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              //color: Colors.red,
+              padding: EdgeInsets.all(15),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.greenAccent,
+                    height: 250,
+                  ),
+                  Container(
+                    child: Text('My product title'),
+                  ),
+                  Container(
+                    child: Text(
+                        'My product title description ,My product title description ,My product title description ,My product title description , '),
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 7.5),
+                          color: Colors.blue,
+                          height: 60,
+                          width: 120,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 7.5),
+                          color: Colors.blue,
+                          height: 60,
+                          width: 120,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
